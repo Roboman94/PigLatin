60df6e348cdd24776ce160b84fac5b57dac9a8be
Kendall <kendallallmand@gmail.com>
2020-04-24 16:48:27 -0400

    second

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Linq;
 
-
-namespace PigLatin
+namespace ConsoleApp8
 {
     class Program
     {
@@ -11,73 +9,98 @@ namespace PigLatin
             bool proceeding = true;
             while (proceeding == true)
             {
-                Console.WriteLine("Enter word here to change to pig latin: ");
-                string input = Console.ReadLine().ToLower();
-                Console.WriteLine();
+                Console.WriteLine("Welcome to the PigLatin generator!");
+                Console.WriteLine("Please enter message");
+                string input = Console.ReadLine().ToLower().Trim();
+                string[] words = input.Split();
+                //Prompts user to enter message
+                //turns input to lowercase and cleans unneccassary spaces
+                //sends message to string array and splits words
 
-                string firstlet = input.Remove(1);
-                string remaining = input.Substring(1);
-                int vowelValid = VowelValid(input, remaining);
-                string remVowel = RemainingIfVowel(input, remaining);
-           
-                if (vowelValid == 1)
+                foreach (string word in words)
                 {
-                    Console.WriteLine(firstlet + remVowel + "way");
+                    TranslateWord(word);
                 }
-                else
-                {
-                    Console.WriteLine(remVowel + firstlet + "ay");
-                }
-
+                //for each word in the message, convert to piglatin
+                //fin
                 proceeding = Proceed();
-
             }
         }
 
-        public static string RemainingIfVowel(string input, string remaining)
+
+        public static void TranslateWord(string input)
         {
-            int i = 0;
+            char[] word = input.ToCharArray();
+            //takes user word and changes it to a char array
+            string output = "";
+            //define output as ""
+            int firstVowelIndex = FirstVowel(word);
+            //Calls FirstVowel method and returns index position of vowel
+            if (firstVowelIndex == 0)
+            //if vowel index starts with a vowel
+            {
+                output = input + "way";
+                //resulting output as input ending in way
+            }
+            else if (firstVowelIndex == -1)
+            //if no vowels are found
+            {
+                output = input;
+                //print output as input
+            }
+            else
+            //if not starting with a vowel
+            {
+                string prefix = input.Substring(firstVowelIndex);
+                
+                //makes string with substring of consonants starting with the first vowel index
+
+                string postfix = input.Substring(0, firstVowelIndex) + "ay";
+               
+                //makes string with substring of consonants before the first vowel index, then ending with ay
+                output = prefix + postfix;
+                //arranges vowel before consonants-ay
+            }
+            
+            Console.Write(output + " ");
+            //result
+        }
+
+        public static int FirstVowel(char[] word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            //loop for length of word array
+            {
+                char letter = word[i];
+                //reassigns letter as current letter in word array
+                if (IsVowel(letter))
+                //if indexed letter is a vowel
+                {
+                    return i;
+                    //return index position of vowel
+                }
+            }
+            Console.WriteLine("Error: Vowel not found. Vowels are necessary for PigLatin!");
+            return -1;
+            //returns error message if no vowels are found
+        }
+
+
+        public static bool IsVowel(char c)
+        {
             char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y' };
-            foreach (char testedchar in vowels)
+            //Creates array of vowels
+
+            foreach (char vowel in vowels)
+            //for each cooresponding vowel
             {
-                if (input.StartsWith(testedchar))
+                if (vowel == c)
+                //if cooresponding vowel matches indexed letter variable
                 {
-
-                    while (remaining.StartsWith(testedchar))
-                    {
-                        remaining = input.Substring(i);
-                        i++;
-                    }
-
-                    return remaining;
+                    return true;
                 }
-                else
-                {
-                    return remaining;
-                }
-
             }
-            return "error";
-        }
-
-        public static int VowelValid(string input, string remaining)
-        {
-            int i = 0;
-            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
-            foreach (char testedchar in vowels)
-            {
-
-
-
-                if (input.StartsWith(testedchar))
-                {
-                    return 1;
-
-                }
-
-
-            }
-            return 0;
+            return false;
         }
 
         public static bool Proceed()
@@ -100,9 +123,6 @@ namespace PigLatin
                 Console.WriteLine("Invalid entry. Please try again");
                 return Proceed();
             }
-
-
         }
-
     }
 }
