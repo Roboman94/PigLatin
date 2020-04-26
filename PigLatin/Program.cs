b7fca833a90ba541df5db13ab1d910d54a178850
Kendall <kendallallmand@gmail.com>
2020-04-26 12:35:18 -0400

    4.26.20

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -111,12 +111,12 @@ namespace ConsoleApp8
         {
             Console.WriteLine();
             Console.WriteLine("Would you like to continue? (y/n) ");
-            string proceed = Console.ReadLine();
-            if (proceed.ToLower().StartsWith("y"))
+            string proceed = Console.ReadLine().ToLower();
+            if (proceed.StartsWith("y"))
             {
                 return true;
             }
-            if (proceed.ToLower().StartsWith("n"))
+            if (proceed.StartsWith("n"))
             {
                 Console.WriteLine("thank you!");
                 Console.WriteLine();
